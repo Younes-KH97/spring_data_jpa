6649d40c0e97ac1139b56a1860c76501954c297e
Younes.khe97 <younes.khellaf97@gmail.com>
2023-04-22 16:03:46 +0100

    add of spring data meth signatures

--- src/main/java/com/cloud/taco/data/IngredientRepository.java
+++ src/main/java/com/cloud/taco/data/IngredientRepository.java
@@ -3,5 +3,6 @@ package com.cloud.taco.data;
 import com.cloud.taco.Ingredient;
 import org.springframework.data.repository.CrudRepository;
 
-public interface IngredientRepository extends CrudRepository<Ingredient, String> {
+public interface IngredientRepository
+        extends CrudRepository<Ingredient, String> {
 }
--- src/main/java/com/cloud/taco/data/OrderRepository.java
+++ src/main/java/com/cloud/taco/data/OrderRepository.java
@@ -1,7 +1,21 @@
 package com.cloud.taco.data;
 
 import com.cloud.taco.TacoOrder;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
-public interface OrderRepository extends CrudRepository<TacoOrder, Long> {
+import java.time.Instant;
+import java.util.List;
+
+public interface OrderRepository
+        extends CrudRepository<TacoOrder, Long> {
+    //Spring Data method signatures
+    
+    List<TacoOrder> findByDeliveryZipAndPlacedAtBetween(String deliveryZid,
+                                                        Instant startDate,
+                                                        Instant finalDate);
+    List<TacoOrder> findByDeliveryCityOrderByDeliveryTo(String city);
+
+    @Query("TacoOrder o where o.deliveryCity='Seattle'")
+    List<TacoOrder> readOrdersDeliveredInSeattle();
 }
