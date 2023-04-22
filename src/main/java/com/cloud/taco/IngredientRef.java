ce221eb489249f73caf476178820263ea38f107d
Younes.khe97 <younes.khellaf97@gmail.com>
2023-04-22 15:22:08 +0100

    add entity classes

---
+++ src/main/java/com/cloud/taco/IngredientRef.java
@@ -0,0 +1,10 @@
+package com.cloud.taco;
+
+import lombok.Data;
+
+@Data
+public class IngredientRef {
+
+    private final String ingredient;
+
+}
---
+++ src/main/java/com/cloud/taco/Taco.java
@@ -0,0 +1,39 @@
+package com.cloud.taco;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.List;
+
+
+@Entity
+
+@AllArgsConstructor
+@NoArgsConstructor
+public class Taco {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private Instant createdAt = Instant.now();
+
+    @NotNull
+    @Size(min=5, message="Name must be at least 5 characters long")
+    private String name;
+
+    @Size(min=1, message="You must choose at least 1 ingredient")
+    @ManyToMany
+    private List<IngredientRef> ingredients = new ArrayList<>();
+
+    public void addIngredient(Ingredient taco) {
+        this.ingredients.add(new IngredientRef(taco.getId()));
+    }
+
+}
