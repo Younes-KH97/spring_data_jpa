4aa2d415d983fe240e47a9c0d169a5ae303db1b7
Younes.khe97 <younes.khellaf97@gmail.com>
2023-04-27 12:10:13 +0100

    authentication support

--- src/main/java/com/cloud/taco/User.java
+++ src/main/java/com/cloud/taco/User.java
@@ -13,6 +13,8 @@ import java.util.Collection;
 @NoArgsConstructor(access= AccessLevel.PRIVATE, force=true)
 public class User implements UserDetails {
 
+    private static final long serialVersionUID = 1L;
+
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
